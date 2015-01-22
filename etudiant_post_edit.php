e41a7eb46b48121b4b19484c3bb09d0472c0d075
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2015-01-22 12:53:03 +0100

    Editing Works - Part 1
    On peut éditer le prénom, le nom et le numéro étudiant pour le moment

--- etudiant_post_edit.php
+++ etudiant_post_edit.php
@@ -1,12 +1,13 @@
 <?php
 require_once 'lib/config.php';
 
+
 $prenom = $_POST['prenom'];
 $nom = $_POST['nom'];
 $num = $_POST['numero_etudiant'];
 $id = $_POST['identifiant'];
 
-$req = $bdd->prepare("UPDATE etudiant SET prenom = '.$prenom.', nom='.$nom.', numero_etudiant='.$num' WHERE identifiant = '.$id.'");
+$req = $bdd->prepare("UPDATE etudiant SET prenom = '$prenom', nom='$nom', numero_etudiant='$num' WHERE identifiant = '$id'");
 $req->execute(array(
     'prenom' => $prenom,
     'nom' => $nom,
