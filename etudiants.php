cad2cbcf3e770aece687f962a0aa099abb9f4a23
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2015-01-22 15:15:46 +0100

    WIP 2

--- etudiants.php
+++ etudiants.php
@@ -18,3 +18,55 @@ $req->execute(array(
 
 header('Location: test.php');
 ?>
+
+
+<?php /*include("../settings/db_connect.php");*/
+
+/* AJOUT*/
+function ajouterMateriel() {
+    global $conn;
+    
+    $sql = "INSERT INTO materiel (nom,reference,numero_cn,numero_proprietaire,description,note,set_id,etat_id,disponibilite_id) VALUES ('Test', 'Référence', 'CN-1770', 'IMAC-13', 'Ceci est un test.', 'Espérons que ça fonctionne', '0', '1', '1')";
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+/*ajouterMateriel();*/
+
+
+/* SELECTION */
+function afficherMateriel() {
+    global $conn;
+    $sql = "SELECT nom, reference, numero_cn FROM materiel";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            echo "<br>Nom : " . $row["nom"]. " - Référence : " . $row["reference"]. " - N° CN : " . $row["numero_cn"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    } 
+}
+/*afficherMateriel();*/
+
+
+/* SUPPRESSION */
+function supprimerMateriel($id) {
+    global $conn;
+    $sql = "DELETE FROM materiel WHERE id=$id";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . mysqli_error($conn);
+    }
+}
+/*supprimerMateriel();*/
+
+
+
+?>
