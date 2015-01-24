293a31cc569b66ee571fae2226345459a324557e
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2015-01-24 15:00:12 +0100

    Requête PHP des étudiants non vérifiés
    Dans le menu latéral, le badge de Gestion des Etudiants indique
    désormais le nombre d'étudiants en attente de validation.

--- edit_etudiant.php
+++ edit_etudiant.php
@@ -19,31 +19,33 @@ $row = mysqli_fetch_assoc(fetchEtudiant($data));
     <div class="container-fluid">
         <div class="row">
             <?php include 'side-menu.php'; ?>
-           <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+            <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
                 <h1 class="page-header">Modifier <?php echo $row['prenom'] . " " . $row['nom']?></h1>
-    <fieldset>
-        <form action="edit_etudiant.php" method="post">
-            <ul>
-               <li>
-                   <input type="hidden" name="identifiant" value="<?php echo $row['identifiant'];?>">
-               </li>
-                <li>
-                    <label for="prenom">Prénom</label>
-                    <input type="text" name ="prenom" value="<?php echo $row['prenom'];?>">
-                    </li>
-                <li>
-                    <label for="nom">Nom</label>
-                    <input type="text" name="nom" value="<?php echo $row['nom'];?>">
-                </li>
-                <li>
-                    <label for="numero_etudiant">Numéro étudiant</label>
-                    <input type="text" name="numero_etudiant" value="<?php echo $row['numero_etudiant'];?>">
-                </li>
-            </ul>
-            <input type="submit" value="Valider" name="submitChange">
-        </form>
-    </fieldset>
-           </div>
+                <fieldset>
+                    <form action="edit_etudiant.php" method="post">
+                        <ul>
+                           <li>
+                               <input type="hidden" name="identifiant" value="<?php echo $row['identifiant'];?>">
+                           </li>
+                            <li>
+                                <label for="prenom">Prénom</label>
+                                <input type="text" name ="prenom" value="<?php echo $row['prenom'];?>">
+                                </li>
+                            <li>
+                                <label for="nom">Nom</label>
+                                <input type="text" name="nom" value="<?php echo $row['nom'];?>">
+                            </li>
+                            <li>
+                                <label for="numero_etudiant">Numéro étudiant</label>
+                                <input type="text" name="numero_etudiant" value="<?php echo $row['numero_etudiant'];?>">
+                            </li>
+                        </ul>
+                        <input type="submit" value="Valider les changements" name="submitChange">
+                        <!--<input type="submit" value="Annuler les changements" name="cancelChange">-->
+                    </form>
+                </fieldset>
+                <h2>Activité</h2>
+            </div>
         </div>
     </div>
         
--- side-menu.php
+++ side-menu.php
@@ -1,27 +1,42 @@
-           <div class="col-sm-3 col-md-2 sidebar">
-             <ul class="nav nav-sidebar">
-                 <li>
-                     <a href>Rendez-vous du jour <span class="badge">3</span></a>
-                 </li>
-                 <li>
-                     <a href>Planning</a>
-                 </li>
-             </ul>  
-             <ul class="nav nav-sidebar">
-                 <li>
-                     <a href>Réservations <span class="badge">4</span></a>
-                 </li>
-             </ul>  
-             <ul class="nav nav-sidebar">
-                 <li><a href>Inventaire</a></li>
-             </ul>
-             <ul class="nav nav-sidebar">
-                 <li><a href="liste_etudiants.php">Gestion des étudiants <span class="badge">2</span></a></li>
-             </ul>
-             <ul class="nav nav-sidebar">
-                 <li><a href>Gestion des projets</a></li>
-             </ul>
-             <ul class="nav nav-sidebar">
-                 <li><a href>Administration Générale</a></li>
-             </ul>  
-           </div>
+<?php
+require_once 'settings/connection.php';
+
+function getNotValidNumber(){
+    global $conn;
+    $sql = "SELECT COUNT(*) FROM etudiant WHERE valide=0";
+    $result = mysqli_query($conn, $sql);
+    return $result;
+}
+
+$dataGNVN = mysqli_fetch_assoc(getNotValidNumber());
+$etudiantsNonVerifies = $dataGNVN['COUNT(*)'];
+?>
+
+
+<div class="col-sm-3 col-md-2 sidebar">
+ <ul class="nav nav-sidebar">
+     <li>
+         <a href>Rendez-vous du jour <span class="badge">3</span></a>
+     </li>
+     <li>
+         <a href>Planning</a>
+     </li>
+ </ul>  
+ <ul class="nav nav-sidebar">
+     <li>
+         <a href>Réservations <span class="badge">4</span></a>
+     </li>
+ </ul>  
+ <ul class="nav nav-sidebar">
+     <li><a href>Inventaire</a></li>
+ </ul>
+ <ul class="nav nav-sidebar">
+     <li><a href="liste_etudiants.php">Gestion des étudiants <span class="badge"><?php echo $etudiantsNonVerifies;?></span></a></li>
+ </ul>
+ <ul class="nav nav-sidebar">
+     <li><a href>Gestion des projets</a></li>
+ </ul>
+ <ul class="nav nav-sidebar">
+     <li><a href>Administration Générale</a></li>
+ </ul>  
+</div>
