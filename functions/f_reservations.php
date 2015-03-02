929c103a8ea83ac544d6c5fce2527a9197692429
Andréas <pinbouen.andreas@gmail.com>
2015-03-02 09:59:01 +0100

    Fetch liste du matériel & étudiant par réservation.

--- functions/f_reservations.php
+++ functions/f_reservations.php
@@ -20,20 +20,34 @@ function afficherReservation($data){
     if(mysqli_num_rows($result) > 0){
         echo "<tbody>";
         while($row = mysqli_fetch_assoc($result)){
+            //Get etudiant
+            $sql_etudiant = "SELECT nom, prenom FROM etudiant WHERE numero_etudiant='$row[etudiant_id]'";
+            $result_etudiant = mysqli_query($conn, $sql_etudiant);
+            $row_etudiant = mysqli_fetch_assoc($result_etudiant);
             echo "<tr>
-                    <td class='col-sm-2'>".$row['date_debut']."</td>
-                    <td class='col-sm-2'>".$row['etudiant_id']."</td>
-                    <td class='col-sm-1'>".$row['reference']."</td>
-                    <td class='col-sm-1'>".$row['raison']."</td>
-                    <td class='col-sm-2'>".$row['date_debut']."</td>
-                    <td class='col-sm-2'>".$row['date_fin']."</td>
-                    <td class='col-sm-2'>
-                        <div class='btn-group'>
-                            <button class='btn btn-default'>Valider</button>
-                            <button class='btn btn-default'>Refuser</button>
-                        </div>
-                    </td>
-                </tr>";
+                        <td class='col-sm-2'>".$row['date_debut']."</td>
+                        <td class='col-sm-2'>".$row_etudiant['prenom']." ".$row_etudiant['nom']."</td>
+                        <td class='col-sm-1'>".$row['reference']."</td>
+                        <td class='col-sm-1'>".$row['raison']."</td>
+                        <td class='col-sm-2'>".$row['date_debut']."</td>
+                        <td class='col-sm-2'>".$row['date_fin']."</td>
+                        <td class='col-sm-2'>
+                            <div class='btn-group'>
+                                <button class='btn btn-default'>Valider</button>
+                                <button class='btn btn-default'>Refuser</button>
+                            </div>
+                        </td>
+                    </tr>
+                <div>
+                    Liste du Matériel : <br>";
+            
+            //Get liste matériel
+            $sql_listeMateriel = "SELECT * FROM detail_emprunt JOIN materiel ON (materiel_id=materiel.id) WHERE reference_id='$row[reference]'";
+            $result_contenu = mysqli_query($conn, $sql_listeMateriel);
+                while($row_listeMateriel = mysqli_fetch_assoc($result_contenu)){
+                    echo $row_listeMateriel['nom']."<br>";   
+                }
+                echo "</div>";
         }
         echo "<tbody>";
     }
--- functions/materiel.php
+++ functions/materiel.php
@@ -152,5 +152,4 @@ function supprimerMateriel() {
     }
 }
 
-
 ?>
