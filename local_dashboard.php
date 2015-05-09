82789e41e94d71165e82e08563a985e499598192
Andréas Pinbouen <pinbouen.andreas@gmail.com>
2015-05-09 22:51:00 +0200

    Ajout espace local
    ADD
    - Liste des réservations du jour
    
    TO DO
    - Actions des boutons

--- local_dashboard.php
+++ local_dashboard.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+require_once 'settings/db_connect.php';
+require_once 'functions/f_reservations.php';
+
 if(!isset($_SESSION['power'])){
 	header('Location:portal.php');
 }
@@ -16,9 +19,11 @@ if(!isset($_SESSION['power'])){
     <?php include 'nav.php'; ?>
    <div class="container-fluid">
       <div class="row">
-          <?php include 'side-menu.php'; ?>
-           <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-               <h1 class="page-header">Please look forward to it - Local Edition™</h1>
+           <div class="col-sm-9 col-sm-offset-3 col-md-8 col-md-offset-2 main">
+               <h1 class="page-header"><span class="glyphicon glyphicon-home"></span> Local</h1>
+               <div class="table-responsive">
+               	<?php afficherEmpruntsProches();?>
+               </div>
            </div>
       </div>
     </div>
